09313d435e5d2beb9592001e12c7d5a4105745f8
Steven de Jong 🍟 <sdejong@cloudsuite.com>
2022-11-15 17:05:52 +0100

    Update sync script and its files

--- sync.sh
+++ sync.sh
@@ -6,27 +6,27 @@ if ! command -v 7z &> /dev/null; then
 fi
 
 
-add () {
-	7z a config $HOME/.config/cura
-	7z a share $HOME/.local/share/cura
+archive () {
+	7z a config "$HOME/.config/cura" -xr@exclude.lst
+	7z a share "$HOME/.local/share/cura" -xr@exclude.lst
 }
 
 
 extract () {
-	7z e "config.7z" -o "$HOME/.config/cura"
-	7z e "share.7z" -o "$HOME/.local/share/cura"
+	7z x "config.7z" -o"$HOME/.config"
+	7z x "share.7z" -o"$HOME/.local/share"
 }
 
 
 
 
-read -p "Extract repo files (else add to zip)? (yes/no) " yn
+read -p "Extract or Archive? (extract/archive) " yn
 case $yn in
-	yes )
+	extract )
 		extract
 	;;
-	no )
-		add
+	archive )
+		archive
 	;;
 	* )
 		echo invalid response
